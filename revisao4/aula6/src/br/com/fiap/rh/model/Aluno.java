96999e8e426ce1d177f18d0f373f2639d4dd3bf2
tigrelau <lauratigreamaral@gmail.com>
2026-03-11 19:56:01 -0300

    add aula06_pacotes_e_modificadores_de_acesso

---
+++ revisao4/aula6/src/br/com/fiap/rh/model/Aluno.java
@@ -0,0 +1,12 @@
+package br.com.fiap.rh.model;
+
+public class Aluno {
+
+    String nome;
+    int idade;
+
+    public void estudar() {
+        System.out.println("Aluno estudando");
+    }
+
+}
---
+++ revisao4/aula6/src/br/com/fiap/rh/teste/Teste.java
@@ -0,0 +1,17 @@
+package br.com.fiap.rh.teste;
+
+import br.com.fiap.rh.model.Aluno;
+
+import java.util.Scanner;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+
+
+    }
+
+
+}
