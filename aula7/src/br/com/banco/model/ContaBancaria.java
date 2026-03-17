960cc332f50d3561a6cce7ec940e9dafee0332c6
tigrelau <lauratigreamaral@gmail.com>
2026-03-16 22:56:39 -0300

    add aula 07.2

--- aula7/src/br/com/banco/model/ContaBancaria.java
+++ aula7/src/br/com/banco/model/ContaBancaria.java
@@ -5,7 +5,7 @@ public class ContaBancaria {
     public int conta;
     public int agencia;
     public int banco;
-    public double saldo;
+    private double saldo;
 
 
     public ContaBancaria(){
@@ -20,6 +20,16 @@ public class ContaBancaria {
         this.banco = banco;
         this.saldo = saldo;
 
+    }
+
+    public void sacar(double valor){
+        this.saldo -= valor;
+        System.out.println("O saldo atual è " + this.saldo + " após os saque é de " + valor);
+    }
+
+    public void depositar(double valor){
+        this.saldo = this.saldo + valor;
+        System.out.println("O saldo atual è " + this.saldo + " após o deposito de " + valor);
     }
 
     public void transferir(int banco, int agencia, int conta, double valor){
@@ -47,4 +57,13 @@ public class ContaBancaria {
     public void imprimir(){
         System.out.print("O saldo na conta " + this.conta + " da agência " + this.agencia + " é" + this.saldo);
     }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
 }
+
+
--- aula7/src/br/com/banco/teste/TesteConstrutor.java
+++ aula7/src/br/com/banco/teste/TesteConstrutor.java
@@ -9,11 +9,26 @@ public class TesteConstrutor {
         conta.agencia = 123;
         conta.banco = 456;
         conta.conta = 789;
-        conta.saldo = 1000.0;
+        conta.setSaldo(1000.0);
+
+        conta.depositar(1000);
+        conta.sacar(100);
+        conta.sacar(200);
+
         conta.imprimir();
 
         ContaBancaria conta2 = new ContaBancaria(321, 654, 987, 2000.0);
+
+        conta.depositar(1000);
+        conta.sacar(100);
+        conta.sacar(200);
+
         conta2.imprimir();
+
+        ContaBancaria conta3 = new ContaBancaria(582, 461, 918, 3000.0);
+        conta2.imprimir();
+
+
 
     }
 
